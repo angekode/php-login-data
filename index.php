b27f5ca67f1b9a0ad09ad54241986772b49e6fc6
Nicolas Gigot <nicolas.gigot@protonmail.com>
2024-12-27 21:03:53 +0000

    Modif: Ajout de fonctions pour l'enregistrement des données.

--- index.php
+++ index.php
@@ -31,9 +31,46 @@
 				return true;
 			}
 
+			function save_user_info_array($userInfoArray) {
+				$jsonDataToSave = json_encode($userInfoArray);
+				if (file_put_contents("names.txt", $jsonDataToSave) == false) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+
+			function load_user_info_array() {
+				$loadedJson = file_get_contents("names.txt");
+				if ($loadedJson === false) {
+					return [];
+				}
+				$userInfoArray = json_decode($loadedJson, true);
+				if ($userInfoArray === null) {
+					return [];
+				}
+
+				return $userInfoArray;
+			}
+
+			function get_user_info($userName, $userInfoArray) {
+				if (empty($userInfoArray) || !array_key_exists($userName, $userInfoArray)) {
+					return [];
+				}
+				return $userInfoArray[$userName];
+			}
+
+			function set_user_info($userInfo, $userInfoArray) {
+				if ($userInfoArray == []) {
+					return false;
+				}
+				$userItemsArray[$userNameSubmitted] = $userInfo;
+				return true;
+			}
+
 			// Nom d'utilisateur et mots de passe fournis ?
-			if (!isset($_POST['user_name']) || !isset($_POST['user_password'])) {
-				echo "<p>Mot de passe ou nom d'utilisateur manquant</p>";
+			if (!isset($_POST['user_name']) || !isset($_POST['user_password']) || !isset($_POST['user_data'])) {
+				echo "<p>Mot de passe, nom d'utilisateur ou données manquantes</p>";
 				exit();
 			}
 
@@ -45,48 +82,41 @@
 
 			$userPasswordSubmitted = $_POST['user_password'];
 			if (!is_valid_entry($userPasswordSubmitted, 16)) {
-				echo "<p>Nom d'utilisateur invalide</p>";
+				echo "<p>Mot de passe invalide</p>";
 				exit();
 			}
 
-			$loadedJson = file_get_contents("names.txt");
-			$userItemsArray = ($loadedJson == false) ? [] : json_decode($loadedJson,true);
-			$userItem = [];
+			$userInfoArray = load_user_info_array();
+			$userInfo = get_user_info($userNameSubmitted, $userInfoArray);
+			echo var_dump($userInfo);
+			// Utilisateur existant, on vérifie le mot de passe
+			if (!empty($userInfo) && !password_verify($userPasswordSubmitted, $userInfo["user_password"])) {
+				echo "<p>Mauvais mot de passe fourni</p>";
+				exit();
 			
-			// Utilisateur existant, on récupère l'item
-			if (!empty($userItemsArray) && array_key_exists($userNameSubmitted, $userItemsArray)) {
-				$userItem = $userItemsArray[$userNameSubmitted];
-				if (!password_verify($userPasswordSubmitted, $userItem["user_password"])) {
-					echo "<p>Mauvais mot de passe fourni</p>";
-					exit();
-				}
-			// Utilisateur non existant, on crée l'item
-			} else {
-				// On stock le hash du mot de passe fourni, attention le hash généré n'est pas le même
-				// à chaque appel, donc il faut utiliser password_verify() le prochain coup.
-				$userItem = ["user_password" => password_hash($userPasswordSubmitted, PASSWORD_DEFAULT)];
-			}
-
-			// Données existantes => affiche
-			if (array_key_exists("user_data",$userItem))  {
-				echo "<p>Salut " . $userNameSubmitted . ", tes données: " . $userItem["user_data"];
 			} 
 
-			// Données fournies => remplace
-			if (isset($_POST['user_data'])) {
-				$userSubmittedData = $_POST['user_data'];
-				if (is_valid_entry($userSubmittedData, 16)) {
-					$userItem["user_data"] = $userSubmittedData;
-					echo "<p>Les données suivantes ont été enregistrées: " . $userSubmittedData;
-				} else {
-					echo "<p>Les données fournies n'ont pas le bon format</p>";
-					exit();
-				}
+			// Utilisateur non existant, on crée l'item
+			if (empty($userInfo)) {
+				// On stock le hash du mot de passe fourni, attention le hash généré n'est pas le même
+				// à chaque appel, donc il faut utiliser password_verify() le prochain coup.
+				$userInfo = ["user_password" => password_hash($userPasswordSubmitted, PASSWORD_DEFAULT)];
+			}
+			
+			// Données existantes => affiche
+			if (array_key_exists("user_data",$userInfo))  {
+				echo "<p>Salut " . $userNameSubmitted . ", tes données: " . $userInfo["user_data"];
 			}
 
-			$userItemsArray[$userNameSubmitted] = $userItem;
-			$jsonDataToSave = json_encode($userItemsArray);
-			file_put_contents("names.txt", $jsonDataToSave);
+			// Données fournies => remplace
+			$userSubmittedData = $_POST['user_data'];
+			if (is_valid_entry($userSubmittedData, 16)) {
+				$userInfo["user_data"] = $userSubmittedData;
+				echo "<p>Les données suivantes ont été enregistrées: " . $userSubmittedData;
+			}
+
+			$userInfoArray[$userNameSubmitted] = $userInfo;
+			save_user_info_array($userInfoArray);
 			echo "<p>Données enregistrées</p>";
 		?>
 	</body>
