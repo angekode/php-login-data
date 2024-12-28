e3309b03851187ce8ab2eb1e1b7b6675b1730ce2
Nicolas Gigot <nicolas.gigot@protonmail.com>
2024-12-28 15:52:39 +0000

    Ajout: Préparation au futur système de login, commit fonctionnel mais comportement pas parfait. - Réorganisation plus modulaire (syntaxe alternative). - Utilisation des exceptions. - Création de cookie pour stocker un token pour une session.

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
 	<head>
 		<title>Test php</title>
@@ -31,9 +33,9 @@
 				return true;
 			}
 
-			function save_user_info_array($userInfoArray) {
-				$jsonDataToSave = json_encode($userInfoArray);
-				if (file_put_contents("names.txt", $jsonDataToSave) == false) {
+			function save_user_info_array($serverSideUsersInfoArray) {
+				$jsonDataToSave = json_encode($serverSideUsersInfoArray);
+				if (file_put_contents("usersinfos.json", $jsonDataToSave) === false) {
 					return false;
 				} else {
 					return true;
@@ -41,83 +43,146 @@
 			}
 
 			function load_user_info_array() {
-				$loadedJson = file_get_contents("names.txt");
+				$loadedJson = file_get_contents("usersinfos.json");
 				if ($loadedJson === false) {
 					return [];
 				}
-				$userInfoArray = json_decode($loadedJson, true);
-				if ($userInfoArray === null) {
+				$serverSideUsersInfoArray = json_decode($loadedJson, true);
+				if ($serverSideUsersInfoArray === null) {
 					return [];
 				}
 
-				return $userInfoArray;
+				return $serverSideUsersInfoArray;
 			}
 
-			function get_user_info($userName, $userInfoArray) {
-				if (empty($userInfoArray) || !array_key_exists($userName, $userInfoArray)) {
+			function get_user_info($userName, $serverSideUsersInfoArray) {
+				if (empty($serverSideUsersInfoArray) || !array_key_exists($userName, $serverSideUsersInfoArray)) {
 					return [];
 				}
-				return $userInfoArray[$userName];
+				return $serverSideUsersInfoArray[$userName];
 			}
 
-			function set_user_info($userInfo, $userInfoArray) {
-				if ($userInfoArray == []) {
+			function set_user_info($userName, $userInfo, $usersInfoArray) {
+				if ($userInfo == []) {
 					return false;
 				}
-				$userItemsArray[$userNameSubmitted] = $userInfo;
+				$usersInfoArray[$userName] = $userInfo;
 				return true;
 			}
 
-			// Nom d'utilisateur et mots de passe fournis ?
-			if (!isset($_POST['user_name']) || !isset($_POST['user_password']) || !isset($_POST['user_data'])) {
-				echo "<p>Mot de passe, nom d'utilisateur ou données manquantes</p>";
-				exit();
+			function save_users_tokens_array($tokensArray) {
+				$jsonString = json_encode($tokensArray);
+				if (file_put_contents("userstokens.json",$jsonString) === false) {
+					return false;
+				} else {
+					return true;
+				}
 			}
 
-			$userNameSubmitted = $_POST['user_name'];
-			if (!is_valid_entry($userNameSubmitted, 16)) {
-				echo "<p>Nom d'utilisateur invalide";
-				exit();
+			function load_users_tokens_array() {
+				$loadedJson = file_get_contents("userstokens.json");
+				if ($loadedJson === false) {
+					return false;
+				}
+				$serverSideTokensArray = json_decode($loadedJson, true);
+				if ($serverSideTokensArray === null) {
+					return [];
+				}
+
+				return $serverSideTokensArray;
 			}
 
-			$userPasswordSubmitted = $_POST['user_password'];
-			if (!is_valid_entry($userPasswordSubmitted, 16)) {
-				echo "<p>Mot de passe invalide</p>";
-				exit();
+			function generate_user_token() {
+				return bin2hex(random_bytes(16));
 			}
 
-			$userInfoArray = load_user_info_array();
-			$userInfo = get_user_info($userNameSubmitted, $userInfoArray);
-			echo var_dump($userInfo);
-			// Utilisateur existant, on vérifie le mot de passe
-			if (!empty($userInfo) && !password_verify($userPasswordSubmitted, $userInfo["user_password"])) {
-				echo "<p>Mauvais mot de passe fourni</p>";
-				exit();
+
+			$messagesToDisplay = [];
 			
-			} 
+			try {
+				$clientSideToken = "";
+				$serverSideTokensArray = [];
+				
+				if (isset($_COOKIE["user_token"])) {
+					$clientSideToken = $_COOKIE["user_token"];
+					$serverSideTokensArray = load_users_tokens_array();
+					if (!empty($serverSideTokensArray) && array_key_exists($clientSideToken, $serverSideTokensArray)) {
+						$serverSideUserName = $serverSideTokensArray[$clientSideToken];
+						array_push($messagesToDisplay, "Re Bonjour " . $serverSideUserName);
+					} else {
+						// token de session non valide
+						$clientSideToken = "";
+					}
+				}
 
-			// Utilisateur non existant, on crée l'item
-			if (empty($userInfo)) {
-				// On stock le hash du mot de passe fourni, attention le hash généré n'est pas le même
-				// à chaque appel, donc il faut utiliser password_verify() le prochain coup.
-				$userInfo = ["user_password" => password_hash($userPasswordSubmitted, PASSWORD_DEFAULT)];
-			}
-			
-			// Données existantes => affiche
-			if (array_key_exists("user_data",$userInfo))  {
-				echo "<p>Salut " . $userNameSubmitted . ", tes données: " . $userInfo["user_data"];
-			}
+				// Nom d'utilisateur et mots de passe fournis ?
+				if (!isset($_POST['user_name']) || !isset($_POST['user_password']) || !isset($_POST['user_data'])) {
+					array_push($messagesToDisplay, "Mot de passe, nom d'utilisateur ou données manquantes");
+					throw new Exception();
+				}
+	
+				$submittedUserName = $_POST['user_name'];
+				if (!is_valid_entry($submittedUserName, 16)) {
+					array_push($messagesToDisplay, "Nom d'utilisateur invalide");
+					throw new Exception();
+				}
+	
+				$submittedPassword = $_POST['user_password'];
+				if (!is_valid_entry($submittedPassword, 16)) {
+					array_push($messagesToDisplay, "Mot de passe invalide");
+					throw new Exception();
+				}
+	
+				$serverSideUsersInfoArray = load_user_info_array();
+				$serverSideUsersInfoArray = get_user_info($submittedUserName, $serverSideUsersInfoArray);
+				
+				// Utilisateur existant, on vérifie le mot de passe
+				if (!empty($serverSideUsersInfoArray) && !password_verify($submittedPassword, $serverSideUsersInfoArray["user_password"])) {
+					array_push($messagesToDisplay, "Mauvais mot de passe fourni");
+					throw new Exception();
+				} 
+	
+				// Utilisateur non existant, on crée l'item
+				if (empty($serverSideUsersInfoArray)) {
+					// On stock le hash du mot de passe fourni, attention le hash généré n'est pas le même
+					// à chaque appel, donc il faut utiliser password_verify() le prochain coup.
+					$serverSideUsersInfoArray = ["user_password" => password_hash($submittedPassword, PASSWORD_DEFAULT)];
+				}
+				
+				// Données existantes => affiche
+				if (array_key_exists("user_data",$serverSideUsersInfoArray))  {
+					array_push($messagesToDisplay, "<p>Salut " . $submittedUserName . ", tes données: " . $serverSideUsersInfoArray["user_data"]);
+				}
+	
+				// Données fournies => remplace
+				$submittedUserData = $_POST['user_data'];
+				if (is_valid_entry($submittedUserData, 16)) {
+					$serverSideUsersInfoArray["user_data"] = $submittedUserData;
+					array_push($messagesToDisplay, "Les données suivantes ont été enregistrées: " . $submittedUserData);
+				}
+	
+				$serverSideUsersInfoArray[$submittedUserName] = $serverSideUsersInfoArray;
+				save_user_info_array($serverSideUsersInfoArray);
+				array_push($messagesToDisplay, "<p>Données enregistrées");
+	
+				if (empty($clientSideToken)) {
+					$clientSideToken = generate_user_token();
+					$serverSideTokensArray = load_users_tokens_array();
+					$serverSideTokensArray[$clientSideToken] = $submittedUserName;
+					if (save_users_tokens_array($serverSideTokensArray) === false) {
+						throw new Exception();
+					}
+					setcookie("user_token",$clientSideToken,time()+3600);
+					array_push($messagesToDisplay, "Nouveau cookie");
+				}
 
-			// Données fournies => remplace
-			$userSubmittedData = $_POST['user_data'];
-			if (is_valid_entry($userSubmittedData, 16)) {
-				$userInfo["user_data"] = $userSubmittedData;
-				echo "<p>Les données suivantes ont été enregistrées: " . $userSubmittedData;
+			} catch(Exception $e) {
+				error_log($e->getMessage());
 			}
-
-			$userInfoArray[$userNameSubmitted] = $userInfo;
-			save_user_info_array($userInfoArray);
-			echo "<p>Données enregistrées</p>";
 		?>
+
+		<?php foreach($messagesToDisplay as $message) : ?>
+			<p><?= $message ?></p>
+		<?php endforeach; ?>
 	</body>
 </html>
